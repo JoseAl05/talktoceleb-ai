6e7588bd22cf2eba8676d9e63591776d6a72940d
JoseAl05 <jparancibialinker@gmail.com>
2023-08-28 19:43:29 -0400

    added a query to get all celebs by category

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,36 @@
 import Categories from '@/components/category/Categories';
+import Celebs from '@/components/celebs/Celebs';
 import SearchInput from '@/components/inputs/SearchInput';
 import prismadb from '@/lib/prismadb';
 import { UserButton } from '@clerk/nextjs';
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+
+    const celebs = await prismadb.celeb.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: 'desc'
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
 
     const categories = await prismadb.category.findMany();
 
@@ -11,6 +38,9 @@ const RootPage = async () => {
         <div className='h-full p-4 space-y-2'>
             <SearchInput />
             <Categories categories={categories} />
+            <Celebs
+                celebs={celebs}
+            />
         </div>
     );
 }
